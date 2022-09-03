8d65d104eabb82d6d5521a025351f92408c59c62
Cris <cong.ntdc@utotech.vn>
2022-09-03 18:58:55 +0700

    update react core

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
-Object.defineProperty(exports, "withGoogleSheets", {
+exports['default'] = void 0;
+Object.defineProperty(exports, 'withGoogleSheets', {
   enumerable: true,
   get: function get() {
-    return _withGoogleSheets["default"];
+    return _withGoogleSheets['default'];
   }
 });
-exports["default"] = void 0;
 
-var _GoogleSheetsProvider = _interopRequireDefault(require("./GoogleSheetsProvider"));
+var _GoogleSheetsProvider = _interopRequireDefault(require('./GoogleSheetsProvider'));
 
-var _withGoogleSheets = _interopRequireDefault(require("./withGoogleSheets"));
+var _withGoogleSheets = _interopRequireDefault(require('./withGoogleSheets'));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-var _default = _GoogleSheetsProvider["default"];
-exports["default"] = _default;
+var _default = _GoogleSheetsProvider['default'];
+exports['default'] = _default;
--- dist/withGoogleSheets.js
+++ dist/withGoogleSheets.js
@@ -1,53 +1,53 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+function _typeof(obj) { '@babel/helpers - typeof'; return _typeof = 'function' == typeof Symbol && 'symbol' == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && 'function' == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? 'symbol' : typeof obj; }, _typeof(obj); }
+
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
-exports["default"] = void 0;
+exports['default'] = void 0;
 
-var _react = _interopRequireWildcard(require("react"));
+var _react = _interopRequireWildcard(require('react'));
 
-var _propTypes = _interopRequireDefault(require("prop-types"));
+var _propTypes = _interopRequireDefault(require('prop-types'));
 
-var _get = _interopRequireDefault(require("lodash/get"));
+var _get = _interopRequireDefault(require('lodash/get'));
 
-var _isArray = _interopRequireDefault(require("lodash/isArray"));
+var _isArray = _interopRequireDefault(require('lodash/isArray'));
 
-var _DefaultLoadErrorComponent = _interopRequireDefault(require("./DefaultLoadErrorComponent"));
+var _DefaultLoadErrorComponent = _interopRequireDefault(require('./DefaultLoadErrorComponent'));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== 'function') return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== 'object' && typeof obj !== 'function') { return { 'default': obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== 'default' && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj['default'] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
-function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, 'prototype', { writable: false }); return Constructor; }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function'); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, 'prototype', { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === 'object' || typeof call === 'function')) { return call; } else if (call !== void 0) { throw new TypeError('Derived constructors may only return object or undefined'); } return _assertThisInitialized(self); }
 
 function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+function _isNativeReflectConstruct() { if (typeof Reflect === 'undefined' || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === 'function') return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -55,9 +55,9 @@ var withGoogleSheets = function withGoogleSheets() {
   var sheets = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '*';
   var config = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
   return function (WrappedComponent) {
-    var _class, _temp;
+    var _class;
 
-    return _temp = _class = function (_Component) {
+    return _class = function (_Component) {
       _inherits(_class, _Component);
 
       var _super = _createSuper(_class);
@@ -73,13 +73,13 @@ var withGoogleSheets = function withGoogleSheets() {
 
         _this = _super.call.apply(_super, [this].concat(args));
 
-        _defineProperty(_assertThisInitialized(_this), "displayName", 'DBGoogleSheets');
+        _defineProperty(_assertThisInitialized(_this), 'displayName', 'DBGoogleSheets');
 
         return _this;
       }
 
       _createClass(_class, [{
-        key: "render",
+        key: 'render',
         value: function render() {
           var result = this.props.db || {};
           var _this$context = this.context,
@@ -91,7 +91,7 @@ var withGoogleSheets = function withGoogleSheets() {
             console.error(error);
             config = _objectSpread(_objectSpread({}, config), {}, {
               dataLoadError: _objectSpread(_objectSpread({}, config.dataLoadError || {}), {}, {
-                title: config.dataLoadError && config.dataLoadError.title ? config.dataLoadError.title : "Data Load Error: HTTP Status: ".concat(error.code),
+                title: config.dataLoadError && config.dataLoadError.title ? config.dataLoadError.title : 'Data Load Error: HTTP Status: '.concat(error.code),
                 message: config.dataLoadError && config.dataLoadError.message ? config.dataLoadError.message : error.message
               })
             });
@@ -99,45 +99,45 @@ var withGoogleSheets = function withGoogleSheets() {
             if (sheets === '*') {
               result = db;
             } else {
-              if (!(0, _isArray["default"])(sheets)) {
+              if (!(0, _isArray['default'])(sheets)) {
                 sheets = [sheets];
               }
 
               sheets.filter(function (s) {
                 return s;
               }).forEach(function (sheet) {
-                var data = (0, _get["default"])(db, sheet);
+                var data = (0, _get['default'])(db, sheet);
 
                 if (data) {
                   result[sheet] = data;
                 } else {
-                  console.error("[METIS]: data for ".concat(sheet, " was empty"));
+                  console.error('[METIS]: data for '.concat(sheet, ' was empty'));
                 }
               });
             }
           }
 
           var errorComponentConfig = config.dataLoadError || {};
-          var LoadErrorComponent = errorComponentConfig.component ? errorComponentConfig.component : _DefaultLoadErrorComponent["default"];
-          return db && result ? _react["default"].createElement(WrappedComponent, _extends({}, this.props, {
+          var LoadErrorComponent = errorComponentConfig.component ? errorComponentConfig.component : _DefaultLoadErrorComponent['default'];
+          return db && result ? _react['default'].createElement(WrappedComponent, _extends({}, this.props, {
             db: result,
             refetch: refetch
-          })) : _react["default"].createElement(LoadErrorComponent, {
+          })) : _react['default'].createElement(LoadErrorComponent, {
             config: errorComponentConfig
           });
         }
       }]);
 
       return _class;
-    }(_react.Component), _defineProperty(_class, "propTypes", {
-      db: _propTypes["default"].object
-    }), _defineProperty(_class, "contextTypes", {
-      db: _propTypes["default"].object,
-      error: _propTypes["default"].object,
-      refetch: _propTypes["default"].func
-    }), _temp;
+    }(_react.Component), _defineProperty(_class, 'propTypes', {
+      db: _propTypes['default'].object
+    }), _defineProperty(_class, 'contextTypes', {
+      db: _propTypes['default'].object,
+      error: _propTypes['default'].object,
+      refetch: _propTypes['default'].func
+    }), _class;
   };
 };
 
 var _default = withGoogleSheets;
-exports["default"] = _default;
+exports['default'] = _default;
